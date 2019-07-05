5968612500b4219c696eaf65ae27a21e119eb687
azu <azuciao@gmail.com>
2019-07-05 15:09:15 +0900

    add

--- packages/reverse/src/reverse.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Reverses the elements in an Array.
- * @param array base array
- */
-export function reverse<T>(array: Array<T>): Array<T> {
-    return [...array].reverse();
-}
--- packages/shift/src/shift.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Removes the first element from an array and returns it.
- */
-export function shift<T>(array: Array<T>): Array<T> {
-    return array.slice(1);
-}
